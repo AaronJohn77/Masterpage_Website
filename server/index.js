b46de96eb674a7feb6ea4d6289a05ca1474769d3
AaronJohn77 <149507664+AaronJohn77@users.noreply.github.com>
2024-04-07 09:01:35 +0800

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,93 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const UserModel = require("./model/Users")
+const bcrypt = require('bcrypt');
+
+const app = express()
+app.use(cors())
+
+const mongodb_name = "FSD_Hackathon"
+const mongoDB_localhost = "mongodb://localhost:27017/"
+const mongoDB_url = mongoDB_localhost + mongodb_name
+
+
+
+app.use(express.json())
+mongoose.connect(mongoDB_url)
+
+
+app.get('/', (req, res) => {
+    UserModel.find({}) //to find the user in database
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+
+})
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      // Find user by email
+      const user = await UserModel.findOne({ email });
+  
+      // If user doesn't exist, return error
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      // Compare passwords
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      // If password is invalid, return error
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid password" });
+      }
+  
+      // Password is valid, return success
+      res.status(200).json({ message: "Login successful", user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+
+app.get('/getUser/:id', (req, res) => {
+    const id = req.params.id //extract the parameters from request url
+    UserModel.findById({ _id:id })
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+
+app.put('/updateUser/:id', (req, res) => {
+    const id = req.params.id
+    UserModel.findByIdAndUpdate({ _id:id }, {//find by id and update the data
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role
+    })
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+
+
+app.delete('/deleteUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({ _id:id })
+        .then(res => res.json(res))
+        .catch(err => res.json(err))
+})
+
+app.post('/createUser', (req, res) => {
+    UserModel.create(req.body)
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+
+})
+
+
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
